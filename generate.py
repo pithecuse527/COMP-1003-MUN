4c07ff149318cb3a05785a46df06b6118a214cab
pithecuse527 <gihong96@gmail.com>
2020-05-24 13:08:18 +0000

    change algorihtms only use one paramter

--- generate.py
+++ generate.py
@@ -24,15 +24,15 @@ def decToBin(x, n):
     return ''.join(str(e) for e in binary)
 ## ================================================================ ##
 
-def recursion_generate(n, i_num_start = 0):
+def top_down_generate(n):
     '''
         1. top-down algorithm
         2. follow up the divide and conquer process
         3. root will have the O(2^n) memory
         4. Cons: Time complexity is O(2^n), which is not efficient
-        5. Pros: Seems n can be putted bigger than dynamic version
+        5. Pros: Seems n can be putted bigger than dynamic version (but why?)
             -> My opinion is since this is a divide and conquer algirhtm, not like dynamic version,
-               everytime the system flow follow divide and conquer, the stack also do append and pop quickly.
+               everytime the system flow follows divide and conquer, the stack also do append and pop quickly.
                Maybe this allows the n can be bigger than 10 but not sure my approach is right.
     '''
     
@@ -40,45 +40,62 @@ def recursion_generate(n, i_num_start = 0):
     
     # base case
     if n == 0:
-        tmp_lst.append(i_num_start)
+        tmp_lst.append(0)
         return tmp_lst
     
-    tmp_lst += recursion_generate(n-1, i_num_start)
-    tmp_lst += recursion_generate(n-1, tmp_lst[-1]+1)
+    # divide and conquer
+    tmp_lst += top_down_generate(n-1)
+    tmp_lst += top_down_generate(n-1)
+    
+    # if we draw the tree, we can see that we should calculate the elements starting from 2^(n-1) to avoid
+    # unnecessary calculation
+    for i in range(pow(2, n-1), len(tmp_lst)):
+        tmp_lst[i] = tmp_lst[i-1] + 1
     
     return tmp_lst
 
 
-def dynamic_generate(n, res_lst = [0]):
+def dynamic_generate(n):
     '''
         1. Bottom-up algorithm (Dynamic programming)
         2. Cons: n is limited as the limit for python3's recursion is 1000 times
         3. Pros: O(n)
     '''
+    res_lst = []
     
-    if len(res_lst) == pow(2,n):
+    if n == 0:
+        res_lst.append(0)
         return res_lst
         
-    res_lst.append(res_lst[-1]+1)
-    return dynamic_generate(n, res_lst)
+    res_lst += dynamic_generate(n-1)
+    
+    # append a element using the previous element
+    for i in range(pow(2, n) - len(res_lst)):
+        res_lst.append(res_lst[-1]+1)
+    
+    return res_lst
     
 def generate(n):
-    print("1. Dynamic programming version")
+    print("The parameter n is -> ", n)
+    print("\n1. Dynamic programming version")
     print("2. Top-down recursive version")
-    v = int(input("Type version... "))
+    v = int(input("Please type the version number... "))
     while v not in [1, 2]:
-        print("Please type 1 or 2")
-        v = input("Type version... ")
+        print("Please type only 1 or 2")
+        v = input("Please type the version number... ")
     
     if v == 1:
-        print("returning list using dynamic programming function")
+        print("\nreturning list using dynamic programming function")
         tmp = dynamic_generate(n)
         for i in range(len(tmp)):
             tmp[i] = decToBin(tmp[i], n)
         return tmp
     else:
-        print("returning list using top-down recursive function")
-        tmp = recursion_generate(n)
+        print("\nreturning list using top-down recursive function")
+        tmp = top_down_generate(n)
         for i in range(len(tmp)):
             tmp[i] = decToBin(tmp[i], n)
         return tmp
+
+tm = generate(5)
+print(tm)
