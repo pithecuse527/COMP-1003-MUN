57959e12c0c2246ea1925f56482da467e1eaf5a3
pithecuse527 <gihong96@gmail.com>
2020-05-25 09:07:53 +0000

    check n can be made by given subset

--- groupsum.py
+++ groupsum.py
@@ -11,4 +11,21 @@
 ##  
 
 def generate(n, subset):
-    pass
+    '''
+        Return boolean value for whether n can be made by subset
+        Time complexity: O(n^2)
+    '''
+    
+    # base case (if the recursive call reaches to the end, with subset containing single element)
+    if len(subset) == 1:
+        return n == subset[0]
+    
+    # case1: using the first element as part of n
+    if generate(n - subset[0], subset[1:]): return True
+    
+    # case2: not using the first element as part of n
+    if generate(n, subset[1:]): return True
+    
+    return False
+
+#print(generate(9, [0, 0, 1, 0, 10, 0, 8, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]))
