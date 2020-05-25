6fed162ac0b2b05298b1fa7d8784f413e110660d
pithecuse527 <gihong96@gmail.com>
2020-05-25 07:57:13 +0000

    initial commit for groupsum judging

---
+++ groupsum.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+##  Assignment-201948932
+##  
+##  Created by Hong Guen Ji on 25/05/20
+##  VIM - Vi IMproved 8.0
+##  Copyright © 2020 Hong Geun Ji. All rights reserved.
+##
+##  Takes a target integer value, n, and a set of integers, 
+##  and determines whether or not any subset of those integers can be summed to make n.
+##  
+
+def generate(n, subset):
+    pass
